5d6d0f5d4947e02709aa386da035850bf33a7807
SoominHeo\HEOSOOMIN <21400810@handong.edu>
2016-11-20 18:36:08 +0900

    no message

--- c.c
+++ c.c
@@ -10,71 +10,68 @@
 #define BUFSIZE 1024
 void error_handling(char *message);
 
-int main(int argc, char **argv){
-        int sock;
-        char message[BUFSIZE], f_name[BUFSIZE];
-        int str_len, addr_size, i, file_size;
-        FILE *fp;
-        char content[BUFSIZE], len[BUFSIZE];
-        int serv_buf_size;
-        struct sockaddr_in serv_addr;
-        struct sockaddr_in from_addr;
+int main(int argc, char **argv) {
+	int sock;
+	char message[BUFSIZE], f_name[BUFSIZE], serv_buf[BUFSIZE];
+	int str_len, i, file_size;
+	socklen_t addr_size;
+	FILE *fp;
+	char content[BUFSIZE], len[BUFSIZE];
+	int serv_buf_size;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in from_addr;
 
-        if(argc!=3){
-                printf("Usage : %s <IP> <port>\n", argv[0]);
-                exit(1);
-        }
-        sock=socket(PF_INET, SOCK_DGRAM, 0);
-        if(sock == -1)
-                error_handling("UDP 소켓 생성 오류");
-        memset(&serv_addr, 0, sizeof(serv_addr));
+	if (argc != 3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == -1)
+		error_handling("UDP 소켓 생성 오류");
+	memset(&serv_addr, 0, sizeof(serv_addr));
 
-        serv_addr.sin_family=AF_INET;
-        serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-        serv_addr.sin_port=htons(atoi(argv[2]));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
 
-// file 이름 입력하고, server에게 file 이름을 보냄
-        fputs("Input file name: ",stdout);
-        scanf("%s", f_name);
 
-        fp = fopen(f_name, "rb");
-        if(NULL == fp){
-                printf("Error opening file\n");
-                return 0;
-        }
-        // 보내기1
-        sendto(sock, f_name, strlen(f_name), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-        // 받기 2
-        memset(message, 0, sizeof(message));
-        str_len = recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr*)&from_addr, &addr_size);
-        printf("sender가 인식한 파일 이름: %s \n", message);
+	fputs("Input file name: ", stdout);
+	scanf("%s", f_name);
+	fp = fopen(f_name, "rb");
+	if (NULL == fp) {
+		printf("Error opening file\n");
+		return 0;
+	}
 
-// server의 버퍼 사이즈 받기
-        // 받기3
-        memset(message, 0, sizeof(message));
-        str_len = recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr*)&from_addr, &addr_size);
-        printf("server의 버퍼 사이즈: %s\n", message);
-        serv_buf_size = atoi(message);
-        printf("숫자로 나타낸 server의 버퍼 사이즈: %d\n", serv_buf_size);
+	sendto(sock, f_name, strlen(f_name), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	memset(message, 0, sizeof(message));
 
-        while (1) {
-                file_size = fread(content, 1, serv_buf_size, fp);
-                if (file_size == 0)
-                        break;
-                sendto(sock, content, serv_buf_size, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-                memset(content, 0, sizeof(content));
-        }
-        strcpy(content, "end");
-        sendto(sock, content, serv_buf_size, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	addr_size = sizeof(from_addr);
+	memset(serv_buf, 0, strlen(serv_buf));
+	str_len = recvfrom(sock, serv_buf, BUFSIZE, 0, (struct sockaddr*)&from_addr, &addr_size);
 
-        fclose(fp);
-        close(sock);
-        return 0;
+	printf("str_len: %d\n", str_len);
+	printf("server의 버퍼 사이즈: %s\n", serv_buf);
+	serv_buf_size = atoi(serv_buf);
+
+	while (1) {
+		file_size = fread(content, 1, serv_buf_size, fp);
+		if (file_size == 0)
+			break;
+		sendto(sock, content, serv_buf_size, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+		memset(content, 0, sizeof(content));
+	}
+	strcpy(content, "end");
+	sendto(sock, content, serv_buf_size, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+	fclose(fp);
+	close(sock);
+	return 0;
 }
 
 void error_handling(char *message)
 {
-        fputs(message, stderr);
-        fputc('\n', stderr);
-        exit(1);
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
--- s.c
+++ s.c
@@ -4,79 +4,175 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <errno.h>
 
 #define BUFSIZE 512
 void error_handling(char *message);
 
-int main(int argc, char **argv){
-        int serv_sock;
-        char message[BUFSIZE], bufsize[BUFSIZE];
-        int str_len;
-        int num=0;
-        FILE *fp;
+typedef struct clnt_buffer {
+	char fileName[BUFSIZE];
+	struct sockaddr_in address;
+	struct clnt_buffer *next;
+}clnt_buffer;
 
-        struct sockaddr_in serv_addr;
-        struct sockaddr_in clnt_addr;
-        int clnt_addr_size;
+typedef struct dummy {
+	clnt_buffer *next;
+	clnt_buffer *end;
+}dummy;
 
-        if(argc!=2){
-                printf("Usage : %s <port>\n", argv[0]);
-                exit(1);
-        }
 
-        serv_sock=socket(PF_INET, SOCK_DGRAM, 0);
-        if(serv_sock == -1){
-                error_handling("UDP 소켓 생성 오류");
-        }
+//create the wait client
+clnt_buffer* create(char* fileName, struct sockaddr_in address);
 
-        memset(&serv_addr, 0, sizeof(serv_addr));
-        serv_addr.sin_family=AF_INET;
-        serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-        serv_addr.sin_port=htons(atoi(argv[1]));
+//make the client wait
+void stop(dummy *buffer, clnt_buffer *new_clnt);
 
-        if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1){
-                error_handling("bind() error");
-        }
-// client가 보낸 파일 이름 출력
-        //받기1
-        str_len = recvfrom(serv_sock, message, BUFSIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-        message[str_len] = 0;
-        printf("client가 보낸 파일: %s \n", message);
-        //보내기2
-        sendto(serv_sock, message, strlen(message), 0, (struct sockaddr*)&clnt_addr, sizeof(clnt_addr));
+//return the next client
+clnt_buffer* start(dummy *buffer);
 
-// client한테 buffer size 보내고 싶어!
-        memset(bufsize, 0, sizeof(bufsize));
-        strcpy(bufsize, "5");
-        printf("server의 buffer size: %s\n", bufsize);
-        //보내기3
-        sendto(serv_sock, bufsize, strlen(bufsize), 0, (struct sockaddr*)&clnt_addr, sizeof(clnt_addr));
-        printf("버퍼사이즈 전송 완료\n");
+int main(int argc, char **argv) {
+	int serv_sock;
+	char message[BUFSIZE];
+	char bufsize[BUFSIZE];
 
-        fp = fopen(message, "wb");
-        if(NULL == fp){
-                printf("Error opening file\n");
-                return 0;
-        }
-        sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_addr, sizeof(clnt_addr));
+	int str_len;
+	int num = 0;
+	FILE *fp;
+	int a;
 
-// 파일 내용 받기
-        while(1) {
-                memset(message, 0, sizeof(message));
-                str_len = recvfrom(serv_sock, message, BUFSIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-                if(strcmp(message, "end")==0) break;
-                message[str_len] = 0;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	int clnt_addr_size;
 
-                fwrite(message, 1, strlen(message), fp);
-        }
+	struct sockaddr_in *curr;
+	struct sockaddr_in *next;
+	dummy* wait_list = NULL;
+	clnt_buffer* clnt_info;
 
-        fclose(fp);
-        return 0;
+	if (argc != 2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (serv_sock == -1) {
+		error_handling("UDP 소켓 생성 오류");
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	curr = NULL;
+	next = NULL;
+	wait_list = (struct dummy*)malloc(sizeof(struct dummy*));
+	wait_list->next == NULL;
+	wait_list->end == NULL;
+
+	if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+		error_handling("bind() error");
+	}
+
+
+	//이제부터 파일 받기 시작!
+	while (1) {
+		clnt_addr_size = sizeof(clnt_addr);
+		str_len = recvfrom(serv_sock, message, BUFSIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+		message[str_len] = 0;
+		curr = next;
+		next = &clnt_addr;
+
+		// 처음
+		if (curr == NULL) {
+			sprintf(bufsize, "%d", BUFSIZE);
+			bufsize[strlen(bufsize)] = 0;
+			a = sendto(serv_sock, bufsize, strlen(bufsize), 0, (struct sockaddr*)&clnt_addr, sizeof(clnt_addr));
+
+			if (a == -1) {
+				perror("sendto");
+				exit(1);
+			}
+
+			//파일 열기
+			printf("%s open\n", message);
+			fp = fopen(message, "wb");
+
+			if (NULL == fp) {
+				printf("Error opening file\n");
+				return 0;
+			}
+			continue;
+		}
+
+		// 파일 다 받았으면
+		if (strcmp(message, "end") == 0) {
+			fclose(fp);
+			if (wait_list->next != NULL) {
+				clnt_info = start(wait_list);
+				clnt_info = start(wait_list);
+				fopen(clnt_info->fileName, "wb");
+				*next = clnt_info->address;
+			}
+			else {
+				curr = NULL;
+				next = NULL;
+			}
+			continue;
+		}
+
+		//다른 client가 보냄
+		if (curr != next) {
+			clnt_info = create(message, clnt_addr);
+			printf("add %s to buffer\n", message);
+			*next = *curr;
+			continue;
+		}
+
+		fwrite(message, 1, strlen(message), fp);
+
+	}
+
+	fclose(fp);
+	return 0;
 }
 
 void error_handling(char *message)
 {
-        fputs(message, stderr);
-        fputc('\n', stderr);
-        exit(1);
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+clnt_buffer* create(char* fileName, struct sockaddr_in address) {
+	clnt_buffer* new_node = (clnt_buffer*)malloc(sizeof(clnt_buffer*));
+	strcpy(new_node->fileName, fileName);
+	new_node->address = address;
+	new_node->next = NULL;
+	return new_node;
+}
+
+void stop(dummy *buffer, clnt_buffer *new_clnt) {
+	clnt_buffer *tmp;
+	if (buffer->next == NULL) {
+		buffer->next = new_clnt;
+		buffer->end = new_clnt;
+	}
+	else {
+		buffer->end->next = new_clnt;
+	}
+}
+
+clnt_buffer* start(dummy *buffer) {
+	clnt_buffer *tmp = NULL;
+	if (buffer->next == NULL) {
+		return;
+	}
+
+	else {
+		tmp = buffer->next;
+		buffer->next = buffer->next->next;
+		tmp->next = NULL;
+		return tmp;
+	}
 }
